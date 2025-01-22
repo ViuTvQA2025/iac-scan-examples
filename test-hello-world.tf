90381b7226b536614b88c3b53ac47d207158398e
ViuTvQA2025 <code.scanning.viutv.2025@gmail.com>
2025-01-22 18:15:44 +0800

    Create test-hello-world.tf

---
+++ test-hello-world.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  version                     = "~> 2.67"
+  region                      = var.region
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  skip_metadata_api_check     = true
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
+}
